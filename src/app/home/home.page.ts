16179995822f5da1996494d88ef6dbf920f4508a
James Marius Santos <130885953+mxrxxsv@users.noreply.github.com>
2024-10-05 23:32:45 +0800

    Update home.page.ts

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,8 +1,7 @@
-import { Component, inject } from '@angular/core';
-import { RefresherCustomEvent } from '@ionic/angular';
-import { MessageComponent } from '../message/message.component';
-
-import { DataService, Message } from '../services/data.service';
+// import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { IonModal } from '@ionic/angular';
+import { OverlayEventDetail } from '@ionic/core/components';
 
 @Component({
   selector: 'app-home',
@@ -10,16 +9,33 @@ import { DataService, Message } from '../services/data.service';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  private data = inject(DataService);
+  @ViewChild(IonModal)
+  modal!: IonModal;
+
   constructor() {}
+  public start_name: string = 'James Marius Santos';
 
-  refresh(ev: any) {
-    setTimeout(() => {
-      (ev as RefresherCustomEvent).detail.complete();
-    }, 3000);
+  public name: string = 'James Marius Santos';
+  public email: string = 'mariusjamess@gmail.com';
+  public phone: string = '09654393608';
+  public gender: string = 'Male';
+
+  cancel() {
+    this.modal.dismiss(null, 'cancel');
   }
 
-  getMessages(): Message[] {
-    return this.data.getMessages();
+  confirm() {
+    this.modal.dismiss(this.name, 'confirm');
   }
+
+  onWillDismiss(event: Event) {
+    const ev = event as CustomEvent<OverlayEventDetail<string>>;
+ 
+  }
+
+  onGenderChange(event: any) {
+    this.gender = event.detail.value;
+  }
+
+
 }
