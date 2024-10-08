94d35b281e77710c62406c4c77ee7933e3b02d86
James Marius Santos <130885953+mxrxxsv@users.noreply.github.com>
2024-10-08 21:43:23 +0800

    Update home.page.ts

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -13,15 +13,42 @@ export class HomePage {
   modal!: IonModal;
 
   constructor() {}
-  public start_name: string = 'James Marius Santos';
+
+  public start_name: string = 'James Marius';
+  public campus: string = "NEUST Sumacab";
+
 
   public name: string = 'James Marius Santos';
   public email: string = 'mariusjamess@gmail.com';
   public phone: string = '09654393608';
   public gender: string = 'Male';
 
+  public updateName: any;
+  public updateEmail: any;
+  public updatePhone: any;
+  public updateGender: any;
+
+
+  public designs: any = [
+    {
+      img: '../../assets/resonate-3.png',
+      name: 'Resonate'
+    }, 
+    {
+      img: '../../assets/kpopify-3.png',
+      name: 'Kpopify'
+    }
+  ]
+
+
   cancel() {
     this.modal.dismiss(null, 'cancel');
+
+    this.updateName = this.name;
+    this.updateEmail = this.email;
+    this.updatePhone = this.phone;
+    this.updateGender = this.gender;
+
   }
 
   confirm() {
