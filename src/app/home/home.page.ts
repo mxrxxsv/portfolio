da66de0c509f71f169c1fae65450d68d393403fd
James Marius Santos <130885953+mxrxxsv@users.noreply.github.com>
2024-10-10 14:52:13 +0800

    Update home.page.ts

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,6 +2,7 @@
 import { Component, ViewChild } from '@angular/core';
 import { IonModal } from '@ionic/angular';
 import { OverlayEventDetail } from '@ionic/core/components';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -12,11 +13,12 @@ export class HomePage {
   @ViewChild(IonModal)
   modal!: IonModal;
 
-  constructor() {}
+  constructor(private router: Router) {}
 
   public start_name: string = 'James Marius';
-  public campus: string = "NEUST Sumacab";
-
+  public campus: string = 'NEUST Sumacab';
+  public year: string = '3rd';
+  public picture: string = '../../assets/oraxol.png';
 
   public name: string = 'James Marius Santos';
   public email: string = 'mariusjamess@gmail.com';
@@ -28,18 +30,21 @@ export class HomePage {
   public updatePhone: any;
   public updateGender: any;
 
+  public withBg: boolean = false;
+  public isWithStyleBind: boolean = false;
 
   public designs: any = [
     {
       img: '../../assets/resonate-3.png',
-      name: 'Resonate'
-    }, 
+      name: 'Resonate',
+      router: './design1',
+    },
     {
       img: '../../assets/kpopify-3.png',
-      name: 'Kpopify'
-    }
-  ]
-
+      name: 'Kpopify',
+      router: './design2',
+    },
+  ];
 
   cancel() {
     this.modal.dismiss(null, 'cancel');
@@ -48,7 +53,6 @@ export class HomePage {
     this.updateEmail = this.email;
     this.updatePhone = this.phone;
     this.updateGender = this.gender;
-
   }
 
   confirm() {
@@ -57,12 +61,45 @@ export class HomePage {
 
   onWillDismiss(event: Event) {
     const ev = event as CustomEvent<OverlayEventDetail<string>>;
- 
   }
 
   onGenderChange(event: any) {
     this.gender = event.detail.value;
   }
 
+  route(route: any) {
+    this.router.navigate([route]);
+  }
 
+  public toggleBG() {
+    this.withBg ? (this.withBg = false) : (this.withBg = true);
+    this.update();
+  }
+
+  public bg: any = {
+    withBackground: this.withBg,
+  };
+
+  public styleSetA: any = {
+    color: 'var(--ion-color-dark)',
+    'border-color': 'var(--ion-color-dark)',
+  };
+
+  public styleSetB: any = {};
+
+  public toggleStyleBinding() {
+    this.isWithStyleBind
+      ? (this.isWithStyleBind = false)
+      : (this.isWithStyleBind = true);
+
+    this.update();
+  }
+
+  public update() {
+    this.bg = {
+      withBackground: this.withBg,
+    };
+
+    this.picture = this.picture = '../../assets/marius.jpg';
+  }
 }
